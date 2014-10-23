f95a2396d630efe5bafd4154d86e3fbe7107aca3
Colin Stolley <cstolley@hushmail.com>
2014-10-22 22:16:56 -0500

    Fixed whitespace; no code changes

--- ircl.c
+++ ircl.c
@@ -27,59 +27,59 @@ eprint(const char *fmt, ...) {
 
 static int
 dial(char *host, char *port) {
-	static struct addrinfo hints;
-	int srv;
-	struct addrinfo *res, *r;
+    static struct addrinfo hints;
+    int srv;
+    struct addrinfo *res, *r;
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	if(getaddrinfo(host, port, &hints, &res) != 0)
-		eprint("error: cannot resolve hostname '%s':", host);
-	for(r = res; r; r = r->ai_next) {
-		if((srv = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1)
-			continue;
-		if(connect(srv, r->ai_addr, r->ai_addrlen) == 0)
-			break;
-		close(srv);
-	}
-	freeaddrinfo(res);
-	if(!r)
-		eprint("error: cannot connect to host '%s'\n", host);
-	return srv;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if(getaddrinfo(host, port, &hints, &res) != 0)
+        eprint("error: cannot resolve hostname '%s':", host);
+    for(r = res; r; r = r->ai_next) {
+        if((srv = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1)
+            continue;
+        if(connect(srv, r->ai_addr, r->ai_addrlen) == 0)
+            break;
+        close(srv);
+    }
+    freeaddrinfo(res);
+    if(!r)
+        eprint("error: cannot connect to host '%s'\n", host);
+    return srv;
 }
 
 #define strlcpy _strlcpy
 static void
 strlcpy(char *to, const char *from, int l) {
-	memccpy(to, from, '\0', l);
-	to[l-1] = '\0';
+    memccpy(to, from, '\0', l);
+    to[l-1] = '\0';
 }
 
 static char *
 eat(char *s, int (*p)(int), int r) {
-	while(*s != '\0' && p(*s) == r)
-		s++;
-	return s;
+    while(*s != '\0' && p(*s) == r)
+        s++;
+    return s;
 }
 
 static char*
 skip(char *s, char c) {
-	while(*s != c && *s != '\0')
-		s++;
-	if(*s != '\0')
-		*s++ = '\0';
-	return s;
+    while(*s != c && *s != '\0')
+        s++;
+    if(*s != '\0')
+        *s++ = '\0';
+    return s;
 }
 
 static void
 trim(char *s) {
-	char *e;
+    char *e;
 
-	e = s + strlen(s) - 1;
-	while(isspace(*e) && e > s)
-		e--;
-	*(e + 1) = '\0';
+    e = s + strlen(s) - 1;
+    while(isspace(*e) && e > s)
+        e--;
+    *(e + 1) = '\0';
 }
 
 static void
@@ -98,10 +98,10 @@ logmsg(const char *msg, const int len) {
 
 static void
 pout(char *channel, char *fmt, ...) {
-	static char timestr[18];
+    static char timestr[18];
     static char logbuf[4096];
-	time_t t;
-	va_list ap;
+    time_t t;
+    va_list ap;
     int saved_point, save_prompt, len;
     char* saved_line;
 
@@ -115,14 +115,14 @@ pout(char *channel, char *fmt, ...) {
         rl_redisplay();
     }
 
-	va_start(ap, fmt);
-	vsnprintf(bufout, sizeof bufout, fmt, ap);
-	va_end(ap);
-	t = time(NULL);
-	strftime(timestr, sizeof timestr, "%D %R", localtime(&t));
+    va_start(ap, fmt);
+    vsnprintf(bufout, sizeof bufout, fmt, ap);
+    va_end(ap);
+    t = time(NULL);
+    strftime(timestr, sizeof timestr, "%D %R", localtime(&t));
 
     fprintf(stdout, "%s : %s %s\n", timestr, channel, bufout);
-	len = snprintf(logbuf, sizeof(logbuf), "%s : %s %s\n", timestr, channel, bufout);
+    len = snprintf(logbuf, sizeof(logbuf), "%s : %s %s\n", timestr, channel, bufout);
     logmsg(logbuf, len);
 
     if (save_prompt) {
@@ -136,45 +136,45 @@ pout(char *channel, char *fmt, ...) {
 
 static void
 sout(char *fmt, ...) {
-	va_list ap;
+    va_list ap;
 
-	va_start(ap, fmt);
-	vsnprintf(bufout, sizeof bufout, fmt, ap);
-	va_end(ap);
-//	fprintf(stdout, "\nSRV: '%s'<END>\n", bufout);
-	fprintf(srv, "%s\r\n", bufout);
+    va_start(ap, fmt);
+    vsnprintf(bufout, sizeof bufout, fmt, ap);
+    va_end(ap);
+//    fprintf(stdout, "\nSRV: '%s'<END>\n", bufout);
+    fprintf(srv, "%s\r\n", bufout);
 }
 
 static void
 privmsg(char *channel, char *msg) {
-	if(channel[0] == '\0') {
-		pout("", "No channel to send to");
-		return;
-	}
-	pout(channel, "<%s> %s", nick, msg);
-	sout("PRIVMSG %s :%s", channel, msg);
+    if(channel[0] == '\0') {
+        pout("", "No channel to send to");
+        return;
+    }
+    pout(channel, "<%s> %s", nick, msg);
+    sout("PRIVMSG %s :%s", channel, msg);
 }
 
 static void
 parsein(char *s) {
-	char c, *p;
+    char c, *p;
     char prompt[128];
 
-	if(s[0] == '\0')
-		return;
-	skip(s, '\n');
-	if(s[0] != '/') {
-		privmsg(channel, s);
-		return;
-	}
-	c = *++s;
+    if(s[0] == '\0')
+        return;
+    skip(s, '\n');
+    if(s[0] != '/') {
+        privmsg(channel, s);
+        return;
+    }
+    c = *++s;
     if (c != '\0' && strlen(s) == 1) {
-		switch(c) {
+        switch(c) {
         case 'c':
             printf("Current channel: %s\n", channel);
             return;
-		case 'h':
-			printf(
+        case 'h':
+            printf(
                  "a - AWAY <msg>\n"
                  "c - show current channel\n"
                  "h - help\n"
@@ -184,82 +184,82 @@ parsein(char *s) {
                  "s - SWITCH <channel>\n"
                  "w - WHO <names>\n"
                  );
-			return;
-		case 'w':
-			sout("WHO *");
-			return;
+            return;
+        case 'w':
+            sout("WHO *");
+            return;
         }
 
     } else if(c != '\0' && isspace(s[1])) {
-		p = s + 2;
-		switch(c) {
-		case 'j':
-			sout("JOIN %s", p);
-		    strlcpy(channel, p, sizeof channel);
+        p = s + 2;
+        switch(c) {
+        case 'j':
+            sout("JOIN %s", p);
+            strlcpy(channel, p, sizeof channel);
             snprintf(prompt, sizeof(prompt), "%s> ", channel);
             rl_set_prompt(prompt);
-			return;
-        case 'a':
-			sout("AWAY %s", p);
             return;
-		case 'w':
-			sout("WHO %s", p);
-			return;
-		case 'l':
-			s = eat(p, isspace, 1);
-			p = eat(s, isspace, 0);
-			if(!*s)
-				s = channel;
-			if(*p)
-				*p++ = '\0';
-			if(!*p)
-				p = "Peace.";
-			sout("PART %s :%s", s, p);
+        case 'a':
+            sout("AWAY %s", p);
+            return;
+        case 'w':
+            sout("WHO %s", p);
+            return;
+        case 'l':
+            s = eat(p, isspace, 1);
+            p = eat(s, isspace, 0);
+            if(!*s)
+                s = channel;
+            if(*p)
+                *p++ = '\0';
+            if(!*p)
+                p = "Peace.";
+            sout("PART %s :%s", s, p);
             snprintf(prompt, sizeof(prompt), "%s> ", channel);
             rl_set_prompt(prompt);
-			return;
-		case 'm':
-			s = eat(p, isspace, 1);
-			p = eat(s, isspace, 0);
-			if(*p)
-				*p++ = '\0';
-			privmsg(s, p);
-			return;
-		case 's':
-			strlcpy(channel, p, sizeof channel);
+            return;
+        case 'm':
+            s = eat(p, isspace, 1);
+            p = eat(s, isspace, 0);
+            if(*p)
+                *p++ = '\0';
+            privmsg(s, p);
+            return;
+        case 's':
+            strlcpy(channel, p, sizeof channel);
             snprintf(prompt, sizeof(prompt), "%s> ", channel);
             rl_set_prompt(prompt);
-			return;
-		}
-	}
-	sout("%s", s);
+            return;
+        }
+    }
+    sout("%s", s);
 }
 
 static void
 parsesrv(char *cmd) {
-	char *usr, *par, *txt;
+    char *usr, *par, *txt;
 
-	usr = host;
-	if(!cmd || !*cmd)
-		return;
-	if(cmd[0] == ':') {
-		usr = cmd + 1;
-		cmd = skip(usr, ' ');
-		if(cmd[0] == '\0')
-			return;
-		skip(usr, '!');
-	}
-	skip(cmd, '\r');
-	par = skip(cmd, ' ');
-	txt = skip(par, ':');
-	trim(par);
-	if(!strcmp("PONG", cmd))
-		return;
-	if(!strcmp("PRIVMSG", cmd))
-		pout(par, "<%s> %s", usr, txt);
-	else if(!strcmp("PING", cmd))
-		sout("PONG %s", txt);
-	else {
+    usr = host;
+    if(!cmd || !*cmd)
+        return;
+    if(cmd[0] == ':') {
+        usr = cmd + 1;
+        cmd = skip(usr, ' ');
+        if(cmd[0] == '\0')
+            return;
+        skip(usr, '!');
+    }
+    skip(cmd, '\r');
+    par = skip(cmd, ' ');
+    txt = skip(par, ':');
+    trim(par);
+    if(!strcmp("PONG", cmd))
+        return;
+    if(!strcmp("PRIVMSG", cmd))
+        pout(par, "<%s> %s", usr, txt);
+    else if(!strcmp("PING", cmd))
+        sout("PONG %s", txt);
+    else {
         if (strcmp(cmd, "JOIN") == 0) {
             if (channel[0] == '\0' && !strcmp(usr, nick)) {
                 char prompt[128];
@@ -274,84 +274,84 @@ parsesrv(char *cmd) {
             /* eat it */
         } else {
             pout(usr, ">< %s (%s): %s", cmd, par, txt);
-    		if(!strcmp("NICK", cmd) && !strcmp(usr, nick))
-    			strlcpy(nick, txt, sizeof nick);
+            if(!strcmp("NICK", cmd) && !strcmp(usr, nick))
+                strlcpy(nick, txt, sizeof nick);
         }
-	}
+    }
 }
 
 int
 main(int argc, char *argv[]) {
-	int i, c;
-	struct timeval tv;
-	const char *user = getenv("USER");
-	fd_set rd;
+    int i, c;
+    struct timeval tv;
+    const char *user = getenv("USER");
+    fd_set rd;
 
-	strlcpy(nick, user ? user : "unknown", sizeof nick);
-	for(i = 1; i < argc; i++) {
-		c = argv[i][1];
-		if(argv[i][0] != '-' || argv[i][2])
-			c = -1;
-		switch(c) {
-		case 'h':
-			if(++i < argc) host = argv[i];
-			break;
-		case 'p':
-			if(++i < argc) port = argv[i];
-			break;
-		case 'n':
-			if(++i < argc) strlcpy(nick, argv[i], sizeof nick);
-			break;
-		case 'k':
-			if(++i < argc) password = argv[i];
-			break;
-		case 'v':
-			eprint("ircl-"VERSION);
-		default:
-			eprint("usage: ircl [-h host] [-p port] [-n nick] [-k keyword] [-v]\n");
-		}
-	}
-	/* init */
-	i = dial(host, port);
-	srv = fdopen(i, "r+");
-	/* login */
-	if(password)
-		sout("PASS %s", password);
-	sout("NICK %s", nick);
-	sout("USER %s localhost %s :%s", nick, host, nick);
-	fflush(srv);
-	setbuf(stdout, NULL);
-	setbuf(srv, NULL);
+    strlcpy(nick, user ? user : "unknown", sizeof nick);
+    for(i = 1; i < argc; i++) {
+        c = argv[i][1];
+        if(argv[i][0] != '-' || argv[i][2])
+            c = -1;
+        switch(c) {
+        case 'h':
+            if(++i < argc) host = argv[i];
+            break;
+        case 'p':
+            if(++i < argc) port = argv[i];
+            break;
+        case 'n':
+            if(++i < argc) strlcpy(nick, argv[i], sizeof nick);
+            break;
+        case 'k':
+            if(++i < argc) password = argv[i];
+            break;
+        case 'v':
+            eprint("ircl-"VERSION);
+        default:
+            eprint("usage: ircl [-h host] [-p port] [-n nick] [-k keyword] [-v]\n");
+        }
+    }
+    /* init */
+    i = dial(host, port);
+    srv = fdopen(i, "r+");
+    /* login */
+    if(password)
+        sout("PASS %s", password);
+    sout("NICK %s", nick);
+    sout("USER %s localhost %s :%s", nick, host, nick);
+    fflush(srv);
+    setbuf(stdout, NULL);
+    setbuf(srv, NULL);
     initialize_readline();
-	for(;;) { /* main loop */
-		FD_ZERO(&rd);
-		FD_SET(0, &rd);
-		FD_SET(fileno(srv), &rd);
-		tv.tv_sec = 120;
-		tv.tv_usec = 0;
-		i = select(fileno(srv) + 1, &rd, 0, 0, &tv);
-		if(i < 0) {
-			if(errno == EINTR)
-				continue;
-			eprint("ircl: error on select():");
-		}
-		else if(i == 0) {
-			if(time(NULL) - trespond >= 300)
-				eprint("ircl shutting down: parse timeout\n");
-			sout("PING %s", host);
-			continue;
-		}
-		if(FD_ISSET(fileno(srv), &rd)) {
-			if(fgets(bufin, sizeof bufin, srv) == NULL)
-				eprint("ircl: remote host closed connection\n");
-			parsesrv(bufin);
-			trespond = time(NULL);
-		}
-		if(FD_ISSET(0, &rd)) {
+    for(;;) { /* main loop */
+        FD_ZERO(&rd);
+        FD_SET(0, &rd);
+        FD_SET(fileno(srv), &rd);
+        tv.tv_sec = 120;
+        tv.tv_usec = 0;
+        i = select(fileno(srv) + 1, &rd, 0, 0, &tv);
+        if(i < 0) {
+            if(errno == EINTR)
+                continue;
+            eprint("ircl: error on select():");
+        }
+        else if(i == 0) {
+            if(time(NULL) - trespond >= 300)
+                eprint("ircl shutting down: parse timeout\n");
+            sout("PING %s", host);
+            continue;
+        }
+        if(FD_ISSET(fileno(srv), &rd)) {
+            if(fgets(bufin, sizeof bufin, srv) == NULL)
+                eprint("ircl: remote host closed connection\n");
+            parsesrv(bufin);
+            trespond = time(NULL);
+        }
+        if(FD_ISSET(0, &rd)) {
             rl_callback_read_char();
         }
     }
-	return 0;
+    return 0;
 }
 
 int
@@ -406,22 +406,22 @@ init_nicks() {
 
 void
 initialize_readline () {
-  rl_readline_name = "ircl";
-  rl_attempted_completion_function = ircl_completion;
+    rl_readline_name = "ircl";
+    rl_attempted_completion_function = ircl_completion;
 
-  /* don't display matches, just complete them in place */
-  rl_parse_and_bind(strdup("TAB:menu-complete"));
+    /* don't display matches, just complete them in place */
+    rl_parse_and_bind(strdup("TAB:menu-complete"));
 
-  /* remove '@&' from defaults */
-  rl_completer_word_break_characters = " \t\n\"\\'`$><=;|{(";
-  rl_callback_handler_install("> ", (rl_vcpfunc_t*) &readline_nonblocking_cb);
-  init_nicks();
+    /* remove '@&' from defaults */
+    rl_completer_word_break_characters = " \t\n\"\\'`$><=;|{(";
+    rl_callback_handler_install("> ", (rl_vcpfunc_t*) &readline_nonblocking_cb);
+    init_nicks();
 }
 
 void
 readline_nonblocking_cb(char* line) {
     if (NULL==line) {
-	    eprint("ircl: broken pipe\n");
+        eprint("ircl: broken pipe\n");
         return;
     }
 
@@ -437,33 +437,33 @@ readline_nonblocking_cb(char* line) {
 
 char **
 ircl_completion(const char *text, int start, int end) {
-  char **matches = NULL;
-  rl_attempted_completion_over = 1;  /* don't match on filenames, etc */
-  matches = rl_completion_matches(text, nick_generator);
-  if (!matches) {
-    matches = rl_completion_matches(text, username_generator);
-  }
-  return matches;
+    char **matches = NULL;
+    rl_attempted_completion_over = 1;  /* don't match on filenames, etc */
+    matches = rl_completion_matches(text, nick_generator);
+    if (!matches) {
+        matches = rl_completion_matches(text, username_generator);
+    }
+    return matches;
 }
 
 char *
 username_generator(const char *text, int state) {
-  static int list_index, len;
-  const char *name;
+    static int list_index, len;
+    const char *name;
 
-  if (!state) {
-      list_index = 0;
-      len = strlen (text);
-  }
+    if (!state) {
+        list_index = 0;
+        len = strlen (text);
+    }
 
-  while ((name = usernames[list_index])) {
-      list_index++;
+    while ((name = usernames[list_index])) {
+        list_index++;
 
-      if (strncasecmp (name, text, len) == 0)
-        return strdup(name);
-  }
-
-  return ((char *)NULL);
+        if (strncasecmp (name, text, len) == 0) {
+            return strdup(name);
+        }
+    }
+    return ((char *)NULL);
 }
 
 char *
@@ -488,18 +488,18 @@ nick_generator(const char *text, int state) {
 
 char*
 stripwhite (char *string) {
-  register char *s, *t;
-
-  for (s = string; whitespace (*s); s++)
-    ;
+    register char *s, *t;
+    for (s = string; whitespace (*s); s++)
+        ;
     
-  if (*s == 0)
-    return (s);
+    if (*s == 0)
+        return (s);
 
-  t = s + strlen (s) - 1;
-  while (t > s && whitespace (*t))
-    t--;
-  *++t = '\0';
+    t = s + strlen (s) - 1;
+    while (t > s && whitespace (*t)) {
+        t--;
+    }
+    *++t = '\0';
 
-  return s;
+    return s;
 }
